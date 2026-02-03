0d296faf3075069e6ac848dcf8eaa1f2650da515
BasicTemplates <kvashnindan@gmail.com>
2026-02-03 19:43:07 +0300

    feat(burger-constructor): added state for price and custom-scroll
    State for price is now working and elements which is unlocked is under custom-scroll, also added
    media queries

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -2,72 +2,92 @@ import {
   ConstructorElement,
   CurrencyIcon,
   Button,
+  DragIcon,
 } from '@krgaa/react-developer-burger-ui-components';
+import { useEffect, useState } from 'react';
 
 import styles from './burger-constructor.module.css';
 
 export const BurgerConstructor = ({ ingredients }) => {
   const bun = ingredients.filter((ingredient) => ingredient.type === 'bun')[0];
+  const ingredientsNoBuns = ingredients.filter(
+    (ingredient) => ingredient.type !== 'bun'
+  );
+
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    let prices = 0;
+    ingredients.forEach((ingredient) => {
+      if (ingredient.type !== 'bun') {
+        prices += ingredient.price;
+      } else {
+        prices += ingredient.price * 2;
+      }
+    });
+    setPrice(prices);
+  }, [ingredients]);
 
   return (
     <section className={`${styles.burger_constructor} ml-10`}>
-      <div className={`${styles.constructor_elements} ml-4`}>
+      <div className="ml-4">
         {bun && (
           <ConstructorElement
             key={bun._id}
             // handleClose={function fee() {}}
             price={bun.price}
-            text={bun.name}
+            text={bun.name + ' (верх)'}
             thumbnail={bun.image}
             type="top"
             isLocked={true}
-            extraClass="mb-4"
+            extraClass={` ${styles.ingredient} ml-8 mb-4`}
           />
         )}
         <div className={`${styles.ingredients} custom-scroll`}>
-          {ingredients &&
-            ingredients.map(
-              (ingredient) =>
-                ingredient.type !== 'bun' && (
-                  <ConstructorElement
-                    key={ingredient._id}
-                    // handleClose={function fee() {}}
-                    price={ingredient.price}
-                    text={ingredient.name}
-                    thumbnail={ingredient.image}
-                    type="normal"
-                    isLocked={false}
-                    extraClass="mb-4"
-                  />
-                )
-            )}
+          {ingredientsNoBuns &&
+            ingredientsNoBuns.map((ingredient) => (
+              <div key={ingredient._id} className={styles.ingredient_drag}>
+                <DragIcon type="primary" className={styles.icon_drag} />
+                <ConstructorElement
+                  // handleClose={function fee() {}}
+                  price={ingredient.price}
+                  text={ingredient.name}
+                  thumbnail={ingredient.image}
+                  type="normal"
+                  isLocked={false}
+                  extraClass={` ${styles.ingredient} ml-2 mb-4`}
+                />
+              </div>
+            ))}
         </div>
         {bun && (
           <ConstructorElement
             key={bun._id}
             // handleClose={function fee() {}}
             price={bun.price}
-            text={bun.name}
+            text={bun.name + ' (низ)'}
             thumbnail={bun.image}
             type="bottom"
             isLocked={true}
-            extraClass=""
+            extraClass={` ${styles.ingredient} ml-8`}
           />
         )}
       </div>
-      <div className={`${styles.order_details} mt-10`}>
-        <div className={`${styles.price} mr-10`}>
-          <div className="text text_type_main-large">610</div>
-          <CurrencyIcon type="primary" />
+      {price ? (
+        <div className={`${styles.order_details} mt-10`}>
+          <div className={`${styles.price} mr-10`}>
+            <div className="text text_type_main-large">{price}</div>
+            <CurrencyIcon type="primary" className={`${styles.icon_price} ml-2`} />
+          </div>
+          <Button
+            // onClick={function fee(){}}
+            size="medium"
+            type="primary"
+          >
+            Оформить заказ
+          </Button>
         </div>
-        <Button
-          // onClick={function fee(){}}
-          size="medium"
-          type="primary"
-        >
-          Оформить заказ
-        </Button>
-      </div>
+      ) : null}
     </section>
   );
 };
